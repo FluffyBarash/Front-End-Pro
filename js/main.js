11a413559d50bd8cdc167e43b4155fea6d116f49
FluffyBarash <paramonov.3022@gmail.com>
2024-01-12 20:12:52 +0200

    HomeWork_1

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const userName = prompt("What is your name?", "John");
+alert(`Hello, ${userName}! How are you?`);
+console.log(`Hello, ${userName}! How are you?`);
+confirm("Do you respect capybaras?");
