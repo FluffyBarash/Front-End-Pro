ded4000b1d396d9137c1336136fa056773ec817a
FluffyBarash <paramonov.3022@gmail.com>
2024-01-16 19:48:20 +0200

    HomeWork_2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-const userName = prompt("What is your name?", "John");
-alert(`Hello, ${userName}! How are you?`);
-console.log(`Hello, ${userName}! How are you?`);
-confirm("Do you respect capybaras?");
+const number_1 = Number(prompt("Ведите число №1"));
+const number_2 = Number(prompt("Ведите число №2"));
+const number_3 = Number(prompt("Ведите число №3"));
+
+const average = (number_1 + number_2 + number_3) / 3;
+alert(`Cреднее арифметическое ${average}`);
+console.log(`Cреднее арифметическое: ${average}`);
