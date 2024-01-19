7bc8a5e347ba453c497b0265cb2dfcf6c2885041
FluffyBarash <paramonov.3022@gmail.com>
2024-01-19 20:34:31 +0200

    HomeWork_3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
+// ##########HomeWork_1####################################################
+
+// const sign = prompt("Ведите число: (+ - * /)");
+// const number_1 = Number(prompt("Ведите число №1"));
+// const number_2 = Number(prompt("Ведите число №2"));
+
+// switch (sign) {
+//   case "+":
+//     answer = number_1 + number_2;
+//     console.log(answer);
+//     alert(answer);
+//     break;
+//   case "-":
+//     answer = number_1 - number_2;
+//     console.log(answer);
+//     alert(answer);
+//     break;
+//   case "*":
+//     answer = number_1 * number_2;
+//     console.log(answer);
+//     alert(answer);
+//     break;
+//   case "/":
+//     answer = number_1 / number_2;
+//     console.log(answer);
+//     alert(answer);
+//     break;
+//   default:
+//     console.log("ОШИБКА!");
+//     alert("ОШИБКА!");
+// }
+
+// ##########HomeWork_2####################################################
+
 const number_1 = Number(prompt("Ведите число №1"));
 const number_2 = Number(prompt("Ведите число №2"));
-const number_3 = Number(prompt("Ведите число №3"));
 
-const average = (number_1 + number_2 + number_3) / 3;
-alert(`Cреднее арифметическое ${average}`);
-console.log(`Cреднее арифметическое: ${average}`);
+const addition = number_1 + number_2;
+const subtraction = number_1 - number_2;
+const multiplication = number_1 * number_2;
+const division = number_1 / number_2;
+
+console.log(
+  `Користувач ввів ${number_1} і ${number_2}: \n ${number_1} + ${number_2} = ${addition} \n ${number_1} - ${number_2} = ${subtraction} \n ${number_1} * ${number_2} = ${multiplication} \n ${number_1} / ${number_2} = ${division}`
+);
+
+alert(
+  `Користувач ввів ${number_1} і ${number_2}: \n ${number_1} + ${number_2} = ${addition} \n ${number_1} - ${number_2} = ${subtraction} \n ${number_1} * ${number_2} = ${multiplication} \n ${number_1} / ${number_2} = ${division}`
+);
