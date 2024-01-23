46f928d6571aa7e764cc3374a7521de3c5d3541b
FluffyBarash <paramonov.3022@gmail.com>
2024-01-23 22:47:36 +0200

    HomeWork_4

--- js/main.js
+++ js/main.js
@@ -1,49 +1,122 @@
-// ##########HomeWork_1####################################################
+// ##########HomeWork-1####################################################
 
-// const sign = prompt("Ведите число: (+ - * /)");
-// const number_1 = Number(prompt("Ведите число №1"));
-// const number_2 = Number(prompt("Ведите число №2"));
+// let numOrStr = prompt("input number or string");
+// console.log(numOrStr);
 
-// switch (sign) {
-//   case "+":
-//     answer = number_1 + number_2;
-//     console.log(answer);
-//     alert(`${number_1} + ${number_2} = ${answer}`);
+// switch (true) {
+//   case numOrStr === null:
+//     console.log("ви скасували");
 //     break;
-//   case "-":
-//     answer = number_1 - number_2;
-//     console.log(answer);
-//     alert(`${number_1} - ${number_2} = ${answer}`);
+//   case numOrStr.trim() === "":
+//     console.log("Empty String");
 //     break;
-//   case "*":
-//     answer = number_1 * number_2;
-//     console.log(answer);
-//     alert(`${number_1} * ${number_2} = ${answer}`);
-//     break;
-//   case "/":
-//     answer = number_1 / number_2;
-//     console.log(answer);
-//     alert(`${number_1} / ${number_2} = ${answer}`);
+//   case isNaN(numOrStr):
+//     console.log(" number is Ba_NaN");
 //     break;
 //   default:
-//     console.log("ОШИБКА!");
-//     alert("ОШИБКА!");
+//     console.log("OK!");
 // }
 
-// ##########HomeWork_2####################################################
+// ##########HomeWork-2####################################################
 
-const number_1 = Number(prompt("Ведите число №1"));
-const number_2 = Number(prompt("Ведите число №2"));
+// 1.
+// let row = "";
+// for (let i = 10; i <= 20; i++) {
+//   row += i + (i < 20 ? ", " : "");
+// }
+// console.log(row);
 
-const addition = number_1 + number_2;
-const subtraction = number_1 - number_2;
-const multiplication = number_1 * number_2;
-const division = number_1 / number_2;
+// 2.
+// let row = "";
+// for (let i = 10; i <= 20; i++) {
+//   let number = i * i;
+//   row += number + (number < 400 ? ", " : "");
+// }
+// console.log(row);
 
-console.log(
-  `Користувач ввів ${number_1} і ${number_2}: \n ${number_1} + ${number_2} = ${addition} \n ${number_1} - ${number_2} = ${subtraction} \n ${number_1} * ${number_2} = ${multiplication} \n ${number_1} / ${number_2} = ${division}`
-);
+// 3.
+// let row = "";
+// for (let i = 1; i <= 10; i++) {
+//   let answer = 7 * i;
+//   row += answer + (answer < 70 ? ", " : "");
+// }
+// console.log(row);
 
-alert(
-  `Користувач ввів ${number_1} і ${number_2}: \n ${number_1} + ${number_2} = ${addition} \n ${number_1} - ${number_2} = ${subtraction} \n ${number_1} * ${number_2} = ${multiplication} \n ${number_1} / ${number_2} = ${division}`
-);
+// 4.
+// let sum = 0;
+// for (let i = 1; i <= 15; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// 5.
+// let sum = 1;
+// for (let i = 15; i <= 35; i++) {
+//   sum *= i;
+// }
+// console.log(sum);
+
+// 6.
+// let sum = 0;
+// for (let i = 1; i <= 500; i++) {
+//   sum += i;
+// }
+
+// console.log(sum / 500);
+
+// 7.
+// let sum = 0;
+// for (let i = 30; i <= 80; i = i + 2) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// 8.
+// let row = "";
+// for (i = 100; i <= 200; i++) {
+//   if (i % 3 == 0) {
+//     row += i + ", ";
+//   }
+// }
+// console.log(row);
+
+// 9.
+// let row = "";
+// let number = 200;
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0) {
+//     row += i + (i < 200 ? ", " : "");
+//   }
+// }
+// console.log(row);
+
+// 10.
+// let row = "";
+// let number = 200;
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0 && i % 2 === 0) {
+//     row += i + (i < 200 ? ", " : "");
+//   }
+// }
+// console.log(row);
+
+// 11.
+// let number = 200;
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//   if (number % i === 0 && i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 12.
+// for (let i = 1; i <= 10; i++) {
+//   let row = "";
+//   for (let y = 1; y <= 10; y++) {
+//     row += i * y + "\t";
+//   }
+//   console.log(row);
+// }
