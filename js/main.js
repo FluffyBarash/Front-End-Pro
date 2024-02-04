0d4962aba2d76c261b314424ed91e7cb36d64155
FluffyBarash <paramonov.3022@gmail.com>
2024-02-04 10:35:34 +0200

    HomeWork_7

--- js/main.js
+++ js/main.js
@@ -1,29 +1,89 @@
-// ##########HomeWork-1####################################################
+// // ##########HomeWork-1####################################################
 
-// function pow(num, degree) {
-//   if (degree == 1) {
-//     return num;
-//   } else {
-//     return num * pow(num, degree - 1);
-//   }
-// }
+// let users = [
+//   {
+//     index: 0,
+//     isActive: true,
+//     balance: "$2,226.60",
+//     name: "Eugenia Sawyer",
+//     gender: "female",
+//     phone: "+1 (840) 583-3207",
+//     address: "949 John Street, Rose, Puerto Rico, 1857",
+//   },
+//   {
+//     index: 1,
+//     isActive: true,
+//     balance: "$2,613.77",
+//     name: "Pauline Gallegos",
+//     gender: "female",
+//     phone: "+1 (985) 593-3328",
+//     address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
+//   },
+//   {
+//     index: 2,
+//     isActive: false,
+//     balance: "$3,976.41",
+//     name: "Middleton Chaney",
+//     gender: "male",
+//     phone: "+1 (995) 591-2478",
+//     address: "807 Fleet Walk, Brutus, Arkansas, 9783",
+//   },
+//   {
+//     index: 3,
+//     isActive: true,
+//     balance: "$1,934.58",
+//     name: "Burns Poole",
+//     gender: "male",
+//     phone: "+1 (885) 559-3422",
+//     address: "730 Seba Avenue, Osage, Alabama, 6290",
+//   },
+//   {
+//     index: 4,
+//     isActive: true,
+//     balance: "$3,261.65",
+//     name: "Mcfadden Horne",
+//     gender: "male",
+//     phone: "+1 (942) 565-3988",
+//     address: "120 Scholes Street, Kirk, Michigan, 1018",
+//   },
+//   {
+//     index: 5,
+//     isActive: false,
+//     balance: "$1,790.56",
+//     name: "Suzette Lewis",
+//     gender: "female",
+//     phone: "+1 (837) 586-3283",
+//     address: "314 Dunne Place, Bawcomville, Guam, 9053",
+//   },
+// ];
 
-// alert(pow(3, 4));
+// let usersFilter = users.filter(
+//   (user) => parseFloat(user.balance.replace(/[^0-9.-]+/g, "")) > 2000
+// );
+// let phoneNumbers = usersFilter.map((user) => user.phone);
+
+// console.log(phoneNumbers);
+
+// let balance = users.reduce(
+//   (total, user) => total + parseFloat(user.balance.replace(/[^0-9.-]+/g, "")),
+//   0
+// );
+
+// console.log(`Общая сумма балансов всех пользователей: ${Math.floor(balance)}`);
 
 // ##########HomeWork-2####################################################
 
-// const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+// function sumFunction() {
+//   let answer = 0;
 
-// function generateKey(quantity, kit) {
-//   let key = "";
-//   const charactersLength = kit.length;
-//   for (i = 0; i < quantity; i++) {
-//     const index = Math.floor(Math.random() * charactersLength);
-//     key = key + kit.charAt(index);
+//   function washingMachine(number) {
+//     answer += number;
+//     console.log(answer);
 //   }
-//   console.log(key);
+//   return washingMachine;
 // }
 
-// generateKey(16, characters);
-
-// ######################################################################
+// let sum = sumFunction();
+// sum(3);
+// sum(5);
+// sum(20);
