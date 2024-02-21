30ec99255620f6df81ccfa3f3899da67fd04501b
FluffyBarash <paramonov.3022@gmail.com>
2024-02-21 22:14:51 +0200

    HomeWork_12

--- js/main.js
+++ js/main.js
@@ -1,105 +1,59 @@
 // ##########_HomeWork_1_####################################################
 
-let prev = document.querySelector("#prev");
-let next = document.querySelector("#next");
-let img = document.querySelector("#img");
-let counter = document.querySelector(".counter");
-let offset = 5;
+const counterOne = document.querySelector("#counter_one");
+const counterTwo = document.querySelector("#counter_two");
+const counterThree = document.querySelector("#counter_three");
+const counterFour = document.querySelector("#counter_four");
+const counterFive = document.querySelector("#counter_five");
 
-prev.addEventListener("click", () => {
-  offset -= 1;
-  console.log(offset);
-  if (offset == 1) {
-    counter.innerText = "1";
-    prev.style.opacity = 0;
-    prev.style.transform = "translateX(-2000px)";
-    img.setAttribute("src", "images/jpg.1.jpg");
-  } else if (offset == 2) {
-    counter.innerText = "2";
-    prev.style.opacity = 1;
-    prev.style.transform = "translateX(0px)";
-    img.setAttribute("src", "images/jpg.2.jpg");
-  } else if (offset == 3) {
-    counter.innerText = "3";
-    img.setAttribute("src", "images/jpg.3.jpg");
-  } else if (offset == 4) {
-    counter.innerText = "4";
-    img.setAttribute("src", "images/jpg.4.jpg");
-  } else if (offset == 5) {
-    counter.innerText = "5";
-    img.setAttribute("src", "images/jpg.5.jpg");
-  } else if (offset == 6) {
-    counter.innerText = "6";
-    img.setAttribute("src", "images/jpg.6.jpg");
-  } else if (offset == 7) {
-    counter.innerText = "7";
-    img.setAttribute("src", "images/jpg.7.jpg");
-  } else if (offset == 8) {
-    counter.innerText = "8";
-    img.setAttribute("src", "images/jpg.8.jpg");
-  } else if (offset == 9) {
-    counter.innerText = "9";
-    img.setAttribute("src", "images/jpg.9.jpg");
-  } else if (offset == 10) {
-    counter.innerText = "10";
-    next.style.opacity = 1;
-    next.style.transform = "translateX(0px)";
-    img.setAttribute("src", "images/jpg.10.jpg");
-  } else if (offset == 11) {
-    counter.innerText = "11";
-    next.style.opacity = 0;
-    next.style.transform = "translateX(2000px)";
-    img.setAttribute("src", "images/jpg.11.jpg");
-  }
+const smileyOne = document.querySelector("#smiley_one");
+const smileyTwo = document.querySelector("#smiley_two");
+const smileyThree = document.querySelector("#smiley_three");
+const smileyFour = document.querySelector("#smiley_four");
+const smileyFive = document.querySelector("#smiley_five");
+
+const button = document.querySelector(".reaction_window__button");
+
+let number1 = 0;
+let number2 = 0;
+let number3 = 0;
+let number4 = 0;
+let number5 = 0;
+
+smileyOne.addEventListener("click", () => {
+  number1 = number1 + 1;
+  counterOne.innerText = number1;
 });
 
-next.addEventListener("click", () => {
-  offset += 1;
-  console.log(offset);
-  if (offset == 1) {
-    counter.innerText = "1";
-    prev.style.opacity = 0;
-    prev.style.transform = "translateX(-2000px)";
-    img.setAttribute("src", "images/jpg.1.jpg");
-  } else if (offset == 2) {
-    counter.innerText = "2";
-    prev.style.opacity = 1;
-    prev.style.transform = "translateX(0px)";
-    img.setAttribute("src", "images/jpg.2.jpg");
-  } else if (offset == 3) {
-    counter.innerText = "3";
-    img.setAttribute("src", "images/jpg.3.jpg");
-  } else if (offset == 4) {
-    counter.innerText = "4";
-    img.setAttribute("src", "images/jpg.4.jpg");
-  } else if (offset == 5) {
-    counter.innerText = "5";
-    img.setAttribute("src", "images/jpg.5.jpg");
-  } else if (offset == 6) {
-    counter.innerText = "6";
-    img.setAttribute("src", "images/jpg.6.jpg");
-  } else if (offset == 7) {
-    counter.innerText = "7";
-    img.setAttribute("src", "images/jpg.7.jpg");
-  } else if (offset == 8) {
-    counter.innerText = "8";
-    img.setAttribute("src", "images/jpg.8.jpg");
-  } else if (offset == 9) {
-    counter.innerText = "9";
-    img.setAttribute("src", "images/jpg.9.jpg");
-  } else if (offset == 10) {
-    counter.innerText = "10";
-    next.style.opacity = 1;
-    next.style.transform = "translateX(0px)";
-    img.setAttribute("src", "images/jpg.10.jpg");
-  } else if (offset == 11) {
-    counter.innerText = "11";
-    next.style.opacity = 0;
-    next.style.transform = "translateX(2000px)";
-    img.setAttribute("src", "images/jpg.11.jpg");
-  }
+smileyTwo.addEventListener("click", () => {
+  number2 = number2 + 1;
+  counterTwo.innerText = number2;
 });
 
-if (offset == 5) {
-  img.setAttribute("src", "images/jpg.5.jpg");
-}
+smileyThree.addEventListener("click", () => {
+  number3 = number3 + 1;
+  counterThree.innerText = number3;
+});
+
+smileyFour.addEventListener("click", () => {
+  number4 = number4 + 1;
+  counterFour.innerText = number4;
+});
+
+smileyFive.addEventListener("click", () => {
+  number5 = number5 + 1;
+  counterFive.innerText = number5;
+});
+
+button.addEventListener("click", () => {
+  number1 = 0;
+  number2 = 0;
+  number3 = 0;
+  number4 = 0;
+  number5 = 0;
+  counterOne.innerText = number1;
+  counterTwo.innerText = number2;
+  counterThree.innerText = number3;
+  counterFour.innerText = number4;
+  counterFive.innerText = number5;
+});
