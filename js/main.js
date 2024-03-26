b4a8954af29f28c4ddf3b80885a94a345f7e330d
FluffyBarash <paramonov.3022@gmail.com>
2024-03-26 16:10:47 +0200

    HomeWork_19

--- js/main.js
+++ js/main.js
@@ -1,66 +1,24 @@
 // // ####################_HomeWork_1_####################
-const windowInfo = document.querySelector(".windowInfo");
 
-const year = prompt("Ваш год рождения?");
-const city = prompt("Ваш город?");
-const sport = prompt(
-  "Выберите ващ любимый вид спорта(Футбол, Баскетбол, Хокей)"
-);
+// const header = document.querySelector(".header");
+// const array = [1, 2, [1.1, 1.2, 1.3], 3];
 
-if (sport == "футбол") {
-  alert("Хочешь стать Криштианом Роналдом?");
-} else if (sport == "баскетбол") {
-  alert("Хочешь стать Майклом Джордоном?");
-} else if (sport == "хокей") {
-  alert("Хочешь стать Александром Овечкиным?");
-}
-
-if (year == "") {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - вы не ввели свой возраст.`;
-  windowInfo.append(yearText);
-} else if (isNaN(year)) {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - вы не ввели свой возраст.`;
-  windowInfo.append(yearText);
-} else {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - ${2024 - year} лет/года.`;
-  windowInfo.append(yearText);
-}
-
-if (city == "") {
-  const cityText = document.createElement("p");
-  cityText.innerText = `Ваш город - вы не ввели свой город.`;
-  windowInfo.append(cityText);
-} else if (city.search(/\d/) != -1) {
-  const cityText = document.createElement("p");
-  cityText.innerText = `Ваш город - вы не ввели свой город .`;
-  windowInfo.append(cityText);
-} else {
-  cityFun();
-}
-
-function cityFun() {
-  if (city == "Киев") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Украины.`;
-    windowInfo.append(cityText);
-  } else if (city == "Париж") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Франции.`;
-    windowInfo.append(cityText);
-  } else if (city == "Лондон") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Великобритании.`;
-    windowInfo.append(cityText);
-  } else if (city == "Вашингтон") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице США.`;
-    windowInfo.append(cityText);
-  } else {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живёшь в городе - ${city}.`;
-    windowInfo.append(cityText);
-  }
-}
+// function generateList(array) {
+//   let ul = document.createElement("ul");
+//   array.forEach((element) => {
+//     if (Array.isArray(element)) {
+//       let ulx2 = document.createElement("ul");
+//       ul.append(ulx2);
+//       element.forEach((item) => {
+//         let li = document.createElement("li");
+//         li.innerText = item;
+//         ulx2.append(li);
+//       });
+//     } else {
+//       let li = document.createElement("li");
+//       li.innerText = element;
+//       ul.append(li);
+//     }
+//     header.append(ul);
+//   });
+// }
