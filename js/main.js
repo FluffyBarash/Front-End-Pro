348db8e4facb0acb72a6d383f30efab4c3e73a81
FluffyBarash <paramonov.3022@gmail.com>
2024-03-29 13:29:25 +0200

    HomeWork_20

--- js/main.js
+++ js/main.js
@@ -1,66 +1,52 @@
 // // ####################_HomeWork_1_####################
-const windowInfo = document.querySelector(".windowInfo");
 
-const year = prompt("Ваш год рождения?");
-const city = prompt("Ваш город?");
-const sport = prompt(
-  "Выберите ващ любимый вид спорта(Футбол, Баскетбол, Хокей)"
-);
+const ext = document.querySelector("#ext");
+const myOrders = document.querySelector("#myOrders");
+const myOrdersWindow = document.querySelector(".myOrdersWindow");
+const addBasket = document.querySelectorAll(".addBasket");
+const card = document.querySelectorAll(".card");
+const empty = document.querySelector("#empty");
+const myOrdersLine = document.querySelector(".myOrdersLine");
+let quantityGoodsInCart = 0;
 
-if (sport == "футбол") {
-  alert("Хочешь стать Криштианом Роналдом?");
-} else if (sport == "баскетбол") {
-  alert("Хочешь стать Майклом Джордоном?");
-} else if (sport == "хокей") {
-  alert("Хочешь стать Александром Овечкиным?");
-}
+myOrders.addEventListener("click", () => {
+  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
+});
 
-if (year == "") {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - вы не ввели свой возраст.`;
-  windowInfo.append(yearText);
-} else if (isNaN(year)) {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - вы не ввели свой возраст.`;
-  windowInfo.append(yearText);
-} else {
-  const yearText = document.createElement("p");
-  yearText.innerText = `Ваш возраст - ${2024 - year} лет/года.`;
-  windowInfo.append(yearText);
-}
+ext.addEventListener("click", () => {
+  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
+});
 
-if (city == "") {
-  const cityText = document.createElement("p");
-  cityText.innerText = `Ваш город - вы не ввели свой город.`;
-  windowInfo.append(cityText);
-} else if (city.search(/\d/) != -1) {
-  const cityText = document.createElement("p");
-  cityText.innerText = `Ваш город - вы не ввели свой город .`;
-  windowInfo.append(cityText);
-} else {
-  cityFun();
-}
+for (let i = 0; i < addBasket.length; i++) {
+  addBasket[i].addEventListener("click", () => {
+    if (addBasket[i].innerText == "Add to Basket") {
+      addBasket[i].innerText = "remove";
+      addBasket[i].classList.toggle("addBasketClassic");
+      addBasket[i].classList.toggle("addBasketRemove");
+      let clone = card[i].cloneNode(true);
+      clone.classList.add(`card-${quantityGoodsInCart}`);
+      myOrdersLine.appendChild(clone);
+      quantityGoodsInCart = quantityGoodsInCart - 1;
+    } else if (addBasket[i].innerText == "remove") {
+      addBasket[i].innerText = "Add to Basket";
+      addBasket[i].classList.toggle("addBasketClassic");
+      addBasket[i].classList.toggle("addBasketRemove");
+      empty.style.display = "inline";
+      quantityGoodsInCart++;
+      let card0 = document.querySelector(".card-0");
+      // let card1 = document.querySelector(".card-1");
+      // let card2 = document.querySelector(".card-2");
+      // let card3 = document.querySelector(".card-3");
+      myOrdersLine.remove(card0);
+      // myOrdersLine.remove(card1);
+      // myOrdersLine.remove(card2);
+      // myOrdersLine.remove(card3);
+    }
 
-function cityFun() {
-  if (city == "Киев") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Украины.`;
-    windowInfo.append(cityText);
-  } else if (city == "Париж") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Франции.`;
-    windowInfo.append(cityText);
-  } else if (city == "Лондон") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице Великобритании.`;
-    windowInfo.append(cityText);
-  } else if (city == "Вашингтон") {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живешь в столице США.`;
-    windowInfo.append(cityText);
-  } else {
-    const cityText = document.createElement("p");
-    cityText.innerText = `Ты живёшь в городе - ${city}.`;
-    windowInfo.append(cityText);
-  }
+    if (quantityGoodsInCart == 0) {
+      empty.style.display = "inline";
+    } else {
+      empty.style.display = "none";
+    }
+  });
 }
