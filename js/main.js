f07e671c59d1addf0d4964fd08f1a72d04c87978
FluffyBarash <paramonov.3022@gmail.com>
2024-03-30 13:01:20 +0200

    HomeWork_21

--- js/main.js
+++ js/main.js
@@ -1,52 +1,49 @@
 // // ####################_HomeWork_1_####################
 
-const ext = document.querySelector("#ext");
-const myOrders = document.querySelector("#myOrders");
-const myOrdersWindow = document.querySelector(".myOrdersWindow");
-const addBasket = document.querySelectorAll(".addBasket");
-const card = document.querySelectorAll(".card");
-const empty = document.querySelector("#empty");
-const myOrdersLine = document.querySelector(".myOrdersLine");
-let quantityGoodsInCart = 0;
-
-myOrders.addEventListener("click", () => {
-  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
-});
-
-ext.addEventListener("click", () => {
-  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
-});
-
-for (let i = 0; i < addBasket.length; i++) {
-  addBasket[i].addEventListener("click", () => {
-    if (addBasket[i].innerText == "Add to Basket") {
-      addBasket[i].innerText = "remove";
-      addBasket[i].classList.toggle("addBasketClassic");
-      addBasket[i].classList.toggle("addBasketRemove");
-      let clone = card[i].cloneNode(true);
-      clone.classList.add(`card-${quantityGoodsInCart}`);
-      myOrdersLine.appendChild(clone);
-      quantityGoodsInCart = quantityGoodsInCart - 1;
-    } else if (addBasket[i].innerText == "remove") {
-      addBasket[i].innerText = "Add to Basket";
-      addBasket[i].classList.toggle("addBasketClassic");
-      addBasket[i].classList.toggle("addBasketRemove");
-      empty.style.display = "inline";
-      quantityGoodsInCart++;
-      let card0 = document.querySelector(".card-0");
-      // let card1 = document.querySelector(".card-1");
-      // let card2 = document.querySelector(".card-2");
-      // let card3 = document.querySelector(".card-3");
-      myOrdersLine.remove(card0);
-      // myOrdersLine.remove(card1);
-      // myOrdersLine.remove(card2);
-      // myOrdersLine.remove(card3);
-    }
-
-    if (quantityGoodsInCart == 0) {
-      empty.style.display = "inline";
-    } else {
-      empty.style.display = "none";
-    }
-  });
+class Human {
+  constructor(name, floor) {
+    this.name = name;
+    this.floor = floor;
+  }
 }
+
+class Apartment {
+  constructor() {
+    this.residents = [];
+  }
+
+  addLiver(human) {
+    this.residents.push(human);
+  }
+}
+
+class House {
+  constructor(capacity) {
+    this.apartments = [];
+    this.capacity = capacity;
+  }
+
+  addApartments(apartment) {
+    if (this.apartments.length < this.capacity) {
+      this.apartments.push(apartment);
+    } else {
+      console.error("Error!");
+    }
+  }
+}
+
+let humanOne = new Human("Bobi", "man");
+let humanTwo = new Human("Elizabeth", "woman");
+
+let apartmentOne = new Apartment();
+let apartmentTwo = new Apartment();
+
+apartmentOne.addLiver(humanOne);
+apartmentTwo.addLiver(humanTwo);
+
+let house = new House(2);
+
+house.addApartments(apartmentOne);
+house.addApartments(apartmentTwo);
+
+console.log(house);
