f6b71537909eb4b37b7632c3b2412194a942ddf8
FluffyBarash <paramonov.3022@gmail.com>
2024-04-14 09:01:54 +0300

    HomeWork_25

--- js/main.js
+++ js/main.js
@@ -1,52 +1,84 @@
 // // ####################_HomeWork_1_####################
 
-const ext = document.querySelector("#ext");
-const myOrders = document.querySelector("#myOrders");
-const myOrdersWindow = document.querySelector(".myOrdersWindow");
-const addBasket = document.querySelectorAll(".addBasket");
-const card = document.querySelectorAll(".card");
-const empty = document.querySelector("#empty");
-const myOrdersLine = document.querySelector(".myOrdersLine");
-let quantityGoodsInCart = 0;
+class Student {
+  constructor(firstName, lastName, birthYear) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.grades = [];
+    this.attendance = new Array(25).fill(null);
+  }
 
-myOrders.addEventListener("click", () => {
-  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
-});
+  addGrade(grade) {
+    this.grades.push(grade);
+  }
 
-ext.addEventListener("click", () => {
-  myOrdersWindow.classList.toggle("myOrdersWindowDelete");
-});
+  getAge() {
+    const currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear;
+  }
 
-for (let i = 0; i < addBasket.length; i++) {
-  addBasket[i].addEventListener("click", () => {
-    if (addBasket[i].innerText == "Add to Basket") {
-      addBasket[i].innerText = "remove";
-      addBasket[i].classList.toggle("addBasketClassic");
-      addBasket[i].classList.toggle("addBasketRemove");
-      let clone = card[i].cloneNode(true);
-      clone.classList.add(`card-${quantityGoodsInCart}`);
-      myOrdersLine.appendChild(clone);
-      quantityGoodsInCart = quantityGoodsInCart - 1;
-    } else if (addBasket[i].innerText == "remove") {
-      addBasket[i].innerText = "Add to Basket";
-      addBasket[i].classList.toggle("addBasketClassic");
-      addBasket[i].classList.toggle("addBasketRemove");
-      empty.style.display = "inline";
-      quantityGoodsInCart++;
-      let card0 = document.querySelector(".card-0");
-      // let card1 = document.querySelector(".card-1");
-      // let card2 = document.querySelector(".card-2");
-      // let card3 = document.querySelector(".card-3");
-      myOrdersLine.remove(card0);
-      // myOrdersLine.remove(card1);
-      // myOrdersLine.remove(card2);
-      // myOrdersLine.remove(card3);
+  getAverageGrade() {
+    if (this.grades.length === 0) {
+      return 0;
     }
+    const sum = this.grades.reduce((acc, grade) => acc + grade, 0);
+    return sum / this.grades.length;
+  }
 
-    if (quantityGoodsInCart == 0) {
-      empty.style.display = "inline";
+  present() {
+    const emptyIndex = this.attendance.findIndex((value) => value === null);
+    if (emptyIndex !== -1) {
+      this.attendance[emptyIndex] = true;
+    }
+  }
+
+  absent() {
+    const emptyIndex = this.attendance.findIndex((value) => value === null);
+    if (emptyIndex !== -1) {
+      this.attendance[emptyIndex] = false;
+    }
+  }
+
+  summary() {
+    const averageGrade = this.getAverageGrade();
+    const attendanceRatio =
+      this.attendance.filter((value) => value === true).length /
+      this.attendance.length;
+
+    if (averageGrade > 90 && attendanceRatio > 0.9) {
+      return "Молодец!";
+    } else if (averageGrade > 0 || attendanceRatio > 0) {
+      return "Хорошо, но можно лучше";
     } else {
-      empty.style.display = "none";
+      return "Редиска!";
     }
-  });
+  }
 }
+
+const student1 = new Student("Иван", "Иванов", 2000);
+student1.addGrade(85);
+student1.addGrade(92);
+student1.present();
+student1.present();
+console.log(
+  `Студент ${student1.firstName} ${
+    student1.lastName
+  }, возраст: ${student1.getAge()} лет`
+);
+console.log(`Средний балл: ${student1.getAverageGrade()}`);
+console.log(`Посещаемость: ${student1.attendance}`);
+console.log(`Оценка: ${student1.summary()}`);
+
+const student2 = new Student("Мария", "Петрова", 2001);
+student2.addGrade(95);
+student2.addGrade(88);
+student2.absent();
+console.log(
+  `Студент ${student2.firstName} ${
+    student2.lastName
+  }, возраст: ${student2.getAge()} лет`
+);
+console.log(`Средний балл: ${student2.getAverageGrade()}`);
+console.log(`Посещаемость: ${student2.attendance}`);
+console.log(`Оценка: ${student2.summary()}`);
