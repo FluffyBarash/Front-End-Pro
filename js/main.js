66ad47b782dce0f6717972b8bb750ff54a812796
FluffyBarash <paramonov.3022@gmail.com>
2024-04-24 17:29:12 +0300

    HomeWork_26

--- js/main.js
+++ js/main.js
@@ -1,84 +1,51 @@
 // // ####################_HomeWork_1_####################
 
-class Student {
-  constructor(firstName, lastName, birthYear) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthYear = birthYear;
-    this.grades = [];
-    this.attendance = new Array(25).fill(null);
+const nameInput = document.querySelector("#nameInput");
+const surname = document.querySelector("#surnameInput");
+const date = document.querySelector("#dateInput");
+const addressTextarea = document.querySelector("#addressTextarea");
+const manRadio = document.querySelector("#manRadioInput");
+const womanRadio = document.querySelector("#womanRadioInput");
+const select = document.querySelector("#select");
+const engInput = document.querySelector("#engCkeckboxInput");
+const ukrInput = document.querySelector("#ukrCkeckboxInput");
+const rusInput = document.querySelector("#rusCkeckboxInput");
+const tyuInput = document.querySelector("#tyuCkeckboxInput");
+const button = document.querySelector("#button");
+const table = document.querySelector(".table");
+// ------------------------------------------------------------
+const nameTable = document.querySelector("#nameTable");
+const surnameTable = document.querySelector("#surnameTable");
+const dateOfBirth = document.querySelector("#dateOfBirth");
+const floor = document.querySelector("#floor");
+const city = document.querySelector("#city");
+const address = document.querySelector("#address");
+const languages = document.querySelector("#languages");
+
+button.addEventListener("click", () => {
+  table.style.zIndex = "2";
+  nameTable.innerText = `${nameInput.value}`;
+  surnameTable.innerText = `${surname.value}`;
+  dateOfBirth.innerText = `${date.value}`;
+  city.innerText = `${select.value}`;
+  address.innerText = `${addressTextarea.value}`;
+  if (manRadio.checked) {
+    floor.innerText = "man";
+  } else if (womanRadio.checked) {
+    floor.innerText = "woman";
   }
-
-  addGrade(grade) {
-    this.grades.push(grade);
+  let activatedLanguages = "";
+  if (engInput.checked) {
+    activatedLanguages += "English ";
   }
-
-  getAge() {
-    const currentYear = new Date().getFullYear();
-    return currentYear - this.birthYear;
+  if (ukrInput.checked) {
+    activatedLanguages += "Ukrainian ";
   }
-
-  getAverageGrade() {
-    if (this.grades.length === 0) {
-      return 0;
-    }
-    const sum = this.grades.reduce((acc, grade) => acc + grade, 0);
-    return sum / this.grades.length;
+  if (rusInput.checked) {
+    activatedLanguages += "Russian ";
   }
-
-  present() {
-    const emptyIndex = this.attendance.findIndex((value) => value === null);
-    if (emptyIndex !== -1) {
-      this.attendance[emptyIndex] = true;
-    }
+  if (tyuInput.checked) {
+    activatedLanguages += "Tyutursky ";
   }
-
-  absent() {
-    const emptyIndex = this.attendance.findIndex((value) => value === null);
-    if (emptyIndex !== -1) {
-      this.attendance[emptyIndex] = false;
-    }
-  }
-
-  summary() {
-    const averageGrade = this.getAverageGrade();
-    const attendanceRatio =
-      this.attendance.filter((value) => value === true).length /
-      this.attendance.length;
-
-    if (averageGrade > 90 && attendanceRatio > 0.9) {
-      return "Молодец!";
-    } else if (averageGrade > 0 || attendanceRatio > 0) {
-      return "Хорошо, но можно лучше";
-    } else {
-      return "Редиска!";
-    }
-  }
-}
-
-const student1 = new Student("Иван", "Иванов", 2000);
-student1.addGrade(85);
-student1.addGrade(92);
-student1.present();
-student1.present();
-console.log(
-  `Студент ${student1.firstName} ${
-    student1.lastName
-  }, возраст: ${student1.getAge()} лет`
-);
-console.log(`Средний балл: ${student1.getAverageGrade()}`);
-console.log(`Посещаемость: ${student1.attendance}`);
-console.log(`Оценка: ${student1.summary()}`);
-
-const student2 = new Student("Мария", "Петрова", 2001);
-student2.addGrade(95);
-student2.addGrade(88);
-student2.absent();
-console.log(
-  `Студент ${student2.firstName} ${
-    student2.lastName
-  }, возраст: ${student2.getAge()} лет`
-);
-console.log(`Средний балл: ${student2.getAverageGrade()}`);
-console.log(`Посещаемость: ${student2.attendance}`);
-console.log(`Оценка: ${student2.summary()}`);
+  languages.innerText = activatedLanguages;
+});
